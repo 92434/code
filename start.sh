04b078c65af6a82740314f8c1bf0fbeec0826029
92434 <31708893+92434@users.noreply.github.com>
2018-03-02 16:17:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cmd=$"/etc/init.d/nessusd start";
-screen -x -S $screen_name -p 0 -X stuff "$cmd"
-screen -x -S $screen_name -p 0 -X stuff $'\n'
+screen -x -S $test1 -p 0 -X stuff "$cmd"
+screen -x -S $test1 -p 0 -X stuff $'\n'
